f1c9ae3018ffcbd4bb8a6723fbe9594c2a154f5c
foysal-mahmud <foysal.bu.cse28@gmail.com>
2020-11-26 20:55:08 -0800

    Setting up Project and Add Login and Signup option

---
+++ App_Login/views.py
@@ -0,0 +1,42 @@
+from django.shortcuts import render
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth import login, authenticate, logout
+from django.urls import reverse
+from django.shortcuts import HttpResponseRedirect
+from django.contrib.auth.decorators import login_required
+
+# Create your views here.
+
+def sign_up(request):
+    form = UserCreationForm()
+    registered = False
+    if request.method == 'POST':
+        form = UserCreationForm(data=request.POST)
+        if form.is_valid():
+            form.save()
+            registered = True
+
+    dict = {'form':form, 'registered':registered}
+    return render(request, 'App_Login/signup.html', context=dict)
+
+
+def login_page(request):
+    form = AuthenticationForm()
+    if request.method == 'POST':
+        form = AuthenticationForm(data=request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+            user = authenticate(username=username, password=password)
+
+            if user is not None:
+                login(request, user)
+                return HttpResponseRedirect(reverse('index'))
+
+    return render(request, 'App_Login/login.html', context={'form':form})
+
+
+@login_required
+def logout_user(request):
+    logout(request)
+    return HttpResponseRedirect(reverse('index'))
